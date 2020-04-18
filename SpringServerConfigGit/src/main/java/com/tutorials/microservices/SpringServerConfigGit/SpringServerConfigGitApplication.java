62e8b0139e7c641888446e5f30b2d12f4c392efa
Kaushiksekar <kaushik3993@gmail.com>
2020-04-18 23:28:02 +0530

    Commit for spring config server with git

---
+++ SpringServerConfigGit/src/main/java/com/tutorials/microservices/SpringServerConfigGit/SpringServerConfigGitApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.microservices.SpringServerConfigGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerConfigGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServerConfigGitApplication.class, args);
+	}
+
+}
---
+++ SpringServerConfigGit/src/test/java/com/tutorials/microservices/SpringServerConfigGit/SpringServerConfigGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.microservices.SpringServerConfigGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerConfigGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
