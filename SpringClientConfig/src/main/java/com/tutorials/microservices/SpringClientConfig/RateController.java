ebe9c3777c1dc533266eccf87aea5b4dcae0080f
Kaushiksekar <kaushik3993@gmail.com>
2020-04-19 19:53:36 +0530

    Commit for finishing refresh when config changes

--- SpringClientConfig/src/main/java/com/tutorials/microservices/SpringClientConfig/RateController.java
+++ SpringClientConfig/src/main/java/com/tutorials/microservices/SpringClientConfig/RateController.java
@@ -1,12 +1,16 @@
 package com.tutorials.microservices.SpringClientConfig;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
+
+    // call refresh url in client if some change happens in config files. This will update the local cache
 
     @Value("${rate}")
     String rate;
